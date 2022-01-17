9a0f709ec086c74e0b7295eddf996b9d9f849f50
Benjmain <73439535+Benjaminfoley@users.noreply.github.com>
2022-01-16 22:12:02 -0800

    completed task 6
    Added the yell

---
+++ yell.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]){
+       
+    if (argc == 0){
+        char line[2048];
+        int counter = read(0, line, sizeof line);
+        counter= write(1,line,counter);
+    }
+    for(int i = 1; i<argc;i++){
+        int fd = open(argv[1], O_RDONLY);
+        char line[2048];
+        int counter = read(fd, line, sizeof line);
+        
+        while(counter>0){
+            write(1,line,counter);
+            counter = read(fd,line,sizeof line);
+            putchar (toupper(argc));
+    close(fd);
+        }
+    }
+
+    return 0;
+}
